abb67eb65cf06dce82c649b9614a3de222a5126b
Abdullah Alhariri <alhariri.abdu.2018@gmail.com>
2024-04-28 05:02:18 +0200

    Cleaning repository and updating swagger config

---
+++ JadeApi/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JadeApi.Controllers;
+
+[Route("Auth")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpGet("Connect")]
+    public async Task<ActionResult> Connect()
+    {
+        return Ok("connect");
+    }
+}
--- JadeApi/Program.cs
+++ JadeApi/Program.cs
@@ -1,11 +1,51 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+const string version = "v0.0.0";
+
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers().AddJsonOptions(o => {
+    o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Jade note taking swagger", Version = version, Description = "Refer to github for docs: https://github.com/jade-note-taking/api"});
+
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Implicit = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri("/auth/connect", UriKind.Relative),
+                Scopes = new Dictionary<string, string>
+                {
+                    { "readAccess", "Access read operations" },
+                    { "writeAccess", "Access write operations" }
+                }
+            }
+        }
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+            },
+            new[] { "readAccess", "writeAccess" }
+        }
+    });
+});
 
 var app = builder.Build();
+app.UseHttpsRedirection(); // Auto redirecting over to https
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,31 +54,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+// app.UseSwaggerUI(c =>
+// {
+//     c.OAuthClientId("test-id");
+//     c.OAuthClientSecret("test-secret");
+//     c.OAuthRealm("test-realm");
+//     c.OAuthAppName("test-app");
+//     c.OAuthScopeSeparator(" ");
+//     c.OAuthAdditionalQueryStringParams(new Dictionary<string, string> { { "foo", "bar" }});
+//     c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+// });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
