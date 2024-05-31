07b9eefb463738934e439336ce50fd427c7890ea
Abdullah Alhariri <alhariri.abdu.2018@gmail.com>
2024-05-31 22:39:47 +0200

    Adding auth0 to project

--- JadeApi/Controllers/AuthController.cs
+++ JadeApi/Controllers/AuthController.cs
@@ -1,14 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JadeApi.Controllers;
 
 [Route("Auth")]
 [ApiController]
-public class AuthController : ControllerBase
+public class AuthController : Controller
 {
-    [HttpGet("Connect")]
-    public async Task<ActionResult> Connect()
+    [Authorize]
+    [HttpGet("Profile")]
+    public IActionResult Profile()
     {
-        return Ok("connect");
+        return Ok(new {
+            Name = User.Identity?.Name,
+            EmailAddress = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
+            ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value
+        });
+    }
+
+    [HttpGet("Login", Name = "Auth.Login")]
+    public async Task Login(string returnUrl = "/")
+    {
+        var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+            .WithRedirectUri(returnUrl)
+            .Build();
+
+        await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    }
+
+    [Authorize]
+    [HttpGet("Logout", Name = "Auth.Logout")]
+    public async Task Logout()
+    {
+        var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+            .WithRedirectUri(Url.Action("Index", "Home"))
+            .Build();
+
+        await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
     }
 }
--- JadeApi/Program.cs
+++ JadeApi/Program.cs
@@ -1,68 +1,75 @@
 using System.Text.Json.Serialization;
+using Auth0.AspNetCore.Authentication;
 using Microsoft.OpenApi.Models;
 
 const string version = "v0.0.0";
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+ options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+// builder.Services.ConfigureSameSiteNoneCookies();
+
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+});
+
 builder.Services.AddControllers().AddJsonOptions(o => {
     o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Jade note taking swagger", Version = version, Description = "Refer to github for docs: https://github.com/jade-note-taking/api"});
 
-    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.OAuth2,
-        Flows = new OpenApiOAuthFlows
-        {
-            Implicit = new OpenApiOAuthFlow
-            {
-                AuthorizationUrl = new Uri("/auth/connect", UriKind.Relative),
-                Scopes = new Dictionary<string, string>
-                {
-                    { "readAccess", "Access read operations" },
-                    { "writeAccess", "Access write operations" }
-                }
-            }
-        }
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
-            },
-            new[] { "readAccess", "writeAccess" }
-        }
-    });
+    // NOTE: oauth2 from swagger docs, add later
+    // c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    // {
+    //     Type = SecuritySchemeType.OAuth2,
+    //     Flows = new OpenApiOAuthFlows
+    //     {
+    //         Implicit = new OpenApiOAuthFlow
+    //         {
+    //             AuthorizationUrl = new Uri("/auth/connect", UriKind.Relative),
+    //             Scopes = new Dictionary<string, string>
+    //             {
+    //                 { "readAccess", "Access read operations" },
+    //                 { "writeAccess", "Access write operations" }
+    //             }
+    //         }
+    //     }
+    // });
+    //
+    // c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    // {
+    //     {
+    //         new OpenApiSecurityScheme
+    //         {
+    //             Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+    //         },
+    //         new[] { "readAccess", "writeAccess" }
+    //     }
+    // });
 });
 
 var app = builder.Build();
-app.UseHttpsRedirection(); // Auto redirecting over to https
+
 app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-// app.UseSwaggerUI(c =>
-// {
-//     c.OAuthClientId("test-id");
-//     c.OAuthClientSecret("test-secret");
-//     c.OAuthRealm("test-realm");
-//     c.OAuthAppName("test-app");
-//     c.OAuthScopeSeparator(" ");
-//     c.OAuthAdditionalQueryStringParams(new Dictionary<string, string> { { "foo", "bar" }});
-//     c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
-// });
 
 app.Run();
