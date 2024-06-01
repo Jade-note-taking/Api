75d6bea2bafcb98ff5d9ef51bd1c4279544143ef
Abdullah Alhariri <alhariri.abdu.2018@gmail.com>
2024-06-01 03:17:19 +0200

    Adding FoldersController.cs and NotesController.cs

---
+++ JadeApi/Controllers/FoldersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JadeApi.Controllers;
+
+[Route("Folders")]
+[ApiController]
+public class FoldersController : Controller
+{
+    [Authorize]
+    [HttpGet]
+    public IActionResult All()
+    {
+        return Ok("All Folders");
+    }
+}
---
+++ JadeApi/Controllers/NotesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JadeApi.Controllers;
+
+[Route("Notes")]
+[ApiController]
+public class NotesController : Controller
+{
+    [Authorize]
+    [HttpGet]
+    public IActionResult All()
+    {
+        return Ok("All Notes");
+    }
+}
